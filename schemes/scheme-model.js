14c80719c94c57ef51109576aaad10602b07cbaf
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-15 16:49:04 -0400

    added the helper functions and the db-config file

---
+++ schemes/scheme-model.js
@@ -0,0 +1,73 @@
+
+const db = require("../data/db-config.js");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first()
+    .then(schemeObj => {
+      // console.log(schemeObj);
+      // schemeObj.steps = ;
+      return db("steps")
+        .where("scheme_id", id)
+        .then(stepsArr => {
+          schemeObj.steps = stepsArr;
+          return schemeObj;
+        });
+
+      // findSteps(schemeObj.id).then(steps => {
+      //   // console.log(steps);
+
+      // });
+    });
+}
+
+function findSteps(id) {
+  return db
+    .select(
+      "steps.id",
+      "steps.step_number",
+      "steps.instructions",
+      "schemes.scheme_name"
+    )
+    .from("steps")
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .where("steps.scheme_id", id)
+    .orderBy("steps.step_number");
+}
+
+function add(scheme) {
+  const { scheme_name } = scheme;
+  return db("schemes")
+    .insert({ scheme_name })
+    .then(([id]) => findById(id));
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes)
+    .then(ids => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  const scheme = findById(id);
+  return db("schemes")
+    .where({ id })
+    .del();
+}
--- server.js
+++ server.js
@@ -7,4 +7,9 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+//sanity test
+server.get('/', (req, res) => {
+    res.status(200).json({api: 'up and running'});
+});
+
 module.exports = server;
